103fbe85a16a58282a6ce73ab1f17ea22b6a8eb4
Topsy123Turvey <stefanturvey1912@gmail.com>
2025-06-06 16:38:50 +0200

    Update header.php with admin links

--- header.php
+++ header.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+$is_admin = false;
+if (isset($_SESSION['user_id'])) {
+    require 'db_connect.php';
+    $sql = "SELECT role FROM users WHERE id = '{$_SESSION['user_id']}'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($result);
+    if ($user['role'] == 'admin') {
+        $is_admin = true;
+    }
+    mysqli_close($conn);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,9 +34,17 @@
                 <li><a href="index.php">Home</a></li>
                 <li><a href="search.php">Search Listings</a></li>
                 <li><a href="add_product.php">List an Item</a></li>
-                <li><a href="signup.php">Sign Up</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="admin.php">Admin Dashboard</a></li>
+                <?php if (!isset($_SESSION['user_id'])) { ?>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Login</a></li>
+                <?php } else { ?>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php } ?>
+                <?php if ($is_admin) { ?>
+                    <li><a href="admin_users.php">Manage Users</a></li>
+                    <li><a href="admin_products.php">Manage Products</a></li>
+                    <li><a href="admin_feedback.php">Manage Feedback</a></li>
+                <?php } ?>
             </ul>
         </nav>
         <script>
@@ -31,4 +53,3 @@
             });
         </script>
     </header>
-     
